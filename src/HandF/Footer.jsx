2d4660d6b5e2f763dccdaf7c0ab189ea9cde6fc6
Hugues <hugues.poncet@yahoo.fr>
2025-01-07 11:13:48 +0100

    Routage fait, header et footer présent, Faire la mise en page avec Sass

---
+++ src/HandF/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <header>
+      <h2>Kasa</h2>
+      <p>2020 Kasa - All rights reserved</p>
+    </header>
+  );
+};
+
+export default Footer;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+const Notfound = () => {
+    return (
+      <div>
+        <h2>Error 404</h2>
+      </div>
+    );
+  };
+  
+  export default Notfound;
