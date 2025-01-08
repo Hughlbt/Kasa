c46a3e79d18776328c777c11cca0818fd9041e54
Hugues <hugues.poncet@yahoo.fr>
2025-01-08 11:03:51 +0100

    Etape 4 presque terminée, banner et cartes présentes, style à finir pour la couleur des cards et le logo du footer

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 function Banner({ title, backgroundImage }) {
     return (
       <div className="banner" style={{ backgroundImage: `url(${backgroundImage})` }}>
+         <div className="banner-overlay"></div>
         <h1>{title}</h1>
       </div>
     );
