8ba49f9dab0a2f92a66f9e6d14b8418ce7a77ab6
Hugues <hugues.poncet@yahoo.fr>
2025-01-09 11:02:44 +0100

    Les informations de la page erreur et la lien de navigation fonctionnelle, reste à faire le style

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom';
+import './NotFound.scss';
 const Notfound = () => {
     return (
-      <div>
-        <h2>Error 404</h2>
+      <div className='notfound'>
+        <h1>404</h1>
+        <h2>Oups! La page que vous demandez n'existe pas</h2>
+        <nav>
+            <Link to="/home">Retourner sur la page d'accueil</Link>
+        </nav>
       </div>
     );
   };
