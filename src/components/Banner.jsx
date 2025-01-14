833ab03fcaaeb201a432f89fcf98b34307969d7f
Hugues <hugues.poncet@yahoo.fr>
2025-01-14 09:28:46 +0100

    Bannières, header et footer réalisés pour la partie responsive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,49 @@
 import PropTypes from 'prop-types';
+import '../style/banner.scss';
+import { useEffect, useState } from 'react';
 
 function Banner({ title, backgroundImage }) {
-    return (
-      <div className="banner" style={{ backgroundImage: `url(${backgroundImage})` }}>
-         <div className="banner-overlay"></div>
-        <h1>{title}</h1>
-      </div>
-    );
-  }
-  
-  Banner.propTypes = {
-    title: PropTypes.string.isRequired,
-    backgroundImage: PropTypes.string.isRequired,
-  };
+  const [isMobile, setIsMobile] = useState(false);
 
-  export default Banner; 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 767);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const parts = title.split(',');
+
+  return (
+    <div className="banner" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="banner-overlay"></div>
+      <h1>
+        {isMobile ? (
+          <>
+            {parts[0]}
+            {parts[1] && (
+              <>
+                ,<br /> {parts[1]?.trim()}
+              </>
+            )}
+          </>
+        ) : (
+          title
+        )}
+      </h1>
+    </div>
+  );
+}
+
+Banner.propTypes = {
+  title: PropTypes.string.isRequired,
+  backgroundImage: PropTypes.string.isRequired,
+};
+
+export default Banner;
