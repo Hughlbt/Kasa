977a3fa2ba20e5c4184c38f5dbb1e276c10c0cd7
Hugues <hugues.poncet@yahoo.fr>
2025-01-16 11:28:45 +0100

    correction apporté à la page notfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,9 @@ const Notfound = () => {
     return (
       <div className='notfound'>
         <h1>404</h1>
-        <h2>Oups! La page que vous demandez n'existe pas</h2>
+        <h2>
+  Oups! La page <span className="break">que</span> vous demandez n'existe pas
+        </h2>
         <nav>
             <Link to="/home">Retourner sur la page d&apos;accueil</Link>
         </nav>
